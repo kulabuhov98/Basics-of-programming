ff0f3161e68a644c5e9c63587186c54154efba5d
Alexander <kulabuhov98@mail.ru>
2022-01-26 15:27:48 +0300

    Create task_3.py

---
+++ Session Winter 2021 - 2022/Practical work # 4/task_3.py
@@ -0,0 +1,24 @@
+# -- coding: utf-8 --
+
+""" 
+    Практическая работа: 4. Номер зачетной книжки: 21-677.
+  
+    Автор: Кулабухов Александр Максимович, ЗИТ-21
+    Дата: 26.01.2022
+
+    Задание 3. Дана строка. Разрежьте ее на две равные части (если длина строки — четная, а если длина строки нечетная, 
+    то длина первой части должна быть на один символ больше). Переставьте эти две части местами, результат запишите в новую строку и выведите на экран. 
+    Решение задачи должно быть выполнено без использования конструкции if и ей подобных.
+"""
+
+sourceString = str(input("Введите строку: "))
+
+def splitAndSwapString(sourceString):
+    sizeString = len(sourceString) // 2 + len(sourceString) % 2
+
+    firstPartString = sourceString[0:sizeString]
+    secondPartString = sourceString[sizeString:]
+
+    print(secondPartString + " " + firstPartString)
+
+splitAndSwapString(sourceString)
