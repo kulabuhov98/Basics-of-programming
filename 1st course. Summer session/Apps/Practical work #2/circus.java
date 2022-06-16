0415f4fc866c15dde8443fc3d41840fed9851ac4
Alexander <kulabuhov98@mail.ru>
2022-06-16 20:40:20 +0300

    Create 1st course. Summer control work\Apps\Practical work #2

---
+++ 1st course. Summer session/Apps/Practical work #2/circus.java
@@ -0,0 +1,52 @@
+package circus;
+
+public class circus {
+    private String title; /* Название представления */
+    private String city; /* Город */
+    private String date; /* Дата премьеры */
+    private String period; /* Период проведения */
+    private Integer price; /* Цена билета */
+    private String author; /* Автор */
+    private String genre; /* Жанр */
+    private Integer numberActors; /* Количество актеров */
+    
+    public circus(String title, String city, String date, String period, Integer price, String author, String genre, Integer numberActors) {
+        this.title = title;
+        this.city = city;
+        this.date = date;
+        this.period = period;
+        this.price = price;
+        this.author = author;
+        this.genre = genre;
+        this.numberActors = numberActors;
+    }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+    
+    public String getCity() { return city; }
+    public void setCity(String city) { this.city = city; }
+    
+    public String getDate() { return date; }
+    public void setDate(String date) { this.date = date; }
+    
+    public String getPeriod() { return period; }
+    public void setPeriod(String period) { this.period = period; }
+    
+    public Integer getPrice() { return price; }
+    public void setPrice(Integer price) { this.price = price; }
+    
+    public String getAuthor() { return author; }
+    public void setAuthor(String author) { this.author = author; }
+    
+    public String getGenre() { return genre; }
+    public void setGenre(String genre) { this.genre = genre; }
+    
+    public Integer getNumberActors() { return numberActors; }
+    public void setNumberActors(Integer numberActors) { this.numberActors = numberActors; }
+    
+    public String toString() {
+        return "Впервые с " + date + " в городе " + city + "! Цирковое представление \"" + title + "\" имени " + author + " и его команды " + numberActors +
+                " актеров в жанре " + genre + "! \n" + "Спешите, период проведения " + period + ". Цена билета от " + price + " рублей.";
+    }
+}
---
+++ 1st course. Summer session/Apps/Practical work #2/demo.java
@@ -0,0 +1,8 @@
+package circus;
+
+public class demo {
+    public static void main(String[] args) {
+        circus performance = new circus("Большое шоу иллюзий", "Воронеж", "18 июня", "с 18 июня по 23 августа", 1500, "Михаил Цителашвили", "иллюзий", 5);
+        System.out.print(performance);
+    }
+}
