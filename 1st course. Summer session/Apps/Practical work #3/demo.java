fbd5ad43cf8d91bc738a3674bbe125c9daa2fb3d
Alexander <kulabuhov98@mail.ru>
2022-06-17 17:57:06 +0300

    Update demo.java

--- 1st course. Summer session/Apps/Practical work #3/demo.java
+++ 1st course. Summer session/Apps/Practical work #3/demo.java
@@ -13,7 +13,7 @@ package circus;
 
 public class demo {
     public static void main(String[] args) {
-        circus performance = new circus("Большое шоу иллюзий", "Воронеж", "18 июня", "с 18 июня по 23 августа", 1500, "Михаил Цителашвили", "иллюзий", 5);
-        System.out.print(performance);
+        circus performance = new circus("Большое шоу иллюзий", "Воронеж", "18 июня", "с 18 июня по 23 августа", 1500, "Михаил Цителашвили", "иллюзий", 5); /* Объект класса */
+        System.out.print(performance); /* Сообщение пользователю */
     }
 }
